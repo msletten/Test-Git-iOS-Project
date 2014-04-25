35c8d217c9095d9aaca9d32470e688e4796fd137
Mat Sletten <matsletten@mmintzmbp.bbbb.net>
2014-04-25 12:15:00 -0700

    added test class

---
+++ Git Test/MSTest.h
@@ -0,0 +1,13 @@
+//
+//  MSTest.h
+//  Git Test
+//
+//  Created by Mat Sletten on 4/25/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSTest : NSObject
+
+@end
---
+++ Git Test/MSTest.m
@@ -0,0 +1,13 @@
+//
+//  MSTest.m
+//  Git Test
+//
+//  Created by Mat Sletten on 4/25/14.
+//  Copyright (c) 2014 Mat Sletten. All rights reserved.
+//
+
+#import "MSTest.h"
+
+@implementation MSTest
+
+@end
